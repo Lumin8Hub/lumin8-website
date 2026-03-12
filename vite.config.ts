4c37b5c946125934682a8cd27ec842b6ff8a3df2
Dan Flatt <108902970+Flattorama@users.noreply.github.com>
2026-03-12 17:49:51 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,11 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.GITHUB_ACTIONS === "true" ? "/lumin8-starter/" : "/",
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
+    allowedHosts: true,
     hmr: {
       overlay: false,
     },
